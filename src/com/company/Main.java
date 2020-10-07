2eb6eaca8f9b11f888243e74b2f96aa40ddb9686
Arturo Negreiros Samanez <anegreiross@outlook.com>
2020-10-06 23:51:12 -0500

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+
+import com.fazecast.jSerialComm.SerialPort;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        SerialPort sp = SerialPort.getCommPort("/dev/ttyACMo");
+	// write your code here
+    }
+}
